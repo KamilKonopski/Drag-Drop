52dd404230fbeae55f5818ac8ac928b409918c24
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-14 00:06:02 +0200

    download inputs and add event listener

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
 
 	constructor() {
 		this.templateElement = document.getElementById(
@@ -14,7 +17,27 @@ class ProjectInput {
 			true
 		);
 		this.element = importedNode.firstElementChild as HTMLFormElement;
+
+		this.titleInputElement = this.element.querySelector(
+			"#title"
+		) as HTMLInputElement;
+		this.descriptionInputElement = this.element.querySelector(
+			"#description"
+		) as HTMLInputElement;
+		this.peopleInputElement = this.element.querySelector(
+			"#people"
+		) as HTMLInputElement;
+
+		this.configure();
 		this.attach();
+	}
+
+	private submitHandler(event: Event) {
+		event.preventDefault();
+	}
+
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler);
 	}
 
 	private attach() {
